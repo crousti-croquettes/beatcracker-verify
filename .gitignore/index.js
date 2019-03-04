1cd04035d4b87fad0641cad8b443c0007efc6131
d0lby <33939409+d0lby@users.noreply.github.com>
2019-03-04 07:19:24 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26,7 +26,7 @@ if (msg.startsWith (prefix + "send")) {
     if (mention == null) { return; }
     message.delete();
     mentionMessage = message.content.slice (7);
-    mention.sendMessage (mentionMessage);
+    mention.sendMessage ("test");
     message.channel.send ("Confirmation envoyée !");
 
 }
