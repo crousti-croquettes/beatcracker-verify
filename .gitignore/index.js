c03b7f81664b949ccc21d00fc9042a918ac5c0b2
d0lby <33939409+d0lby@users.noreply.github.com>
2019-03-05 20:37:25 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,47 +2,41 @@ const Discord = require ('discord.js');
 
 var client = new Discord.Client();
 
-function getAllMethods(object) {
-    return Object.getOwnPropertyNames(object).filter(function(property) {
-        return typeof object[property] == 'function';
-    });
-}
 client.login(process.env.TOKEN)
 
 client.on ("ready",() => {
 
 console.log("ready!");
-//console.log(client.member)
+
 client.user.setActivity("created by sneaky");
 
 });
 
 client.on("guildMemberAdd", function(member)
 {
-  let memberRole = message.member.guild.roles.find("id", "552549087476711444");
-//console.log(message.author); 
-console.log("here");
-   message.author.addRole(memberRole.name);  
+  let memberRole = message.member.guild.roles.find("name", "Producteur ✔️");
+    message.member.addRole(memberRole);  
 });    
 
 const prefix = "b!";
 
 client.on ("message",(message) => {
+
     msg = message.content.toLowerCase();
+
     if (message.author.bot) return;
 
     mention = message.mentions.users.first();
 
 if (msg.startsWith (prefix + "send")) {
-    if (mention == null) { console.log(123); }
+    if (mention == null) { return; }
     message.delete();
     mentionMessage = message.content.slice (7);
-    message.author.sendMessage (message.author.username + " Vous avez été validé, vous êtes maintenant producteur confirmé.");
+    mention.sendMessage (mention + " Vous avez été validé, vous êtes maintenant producteur confirmé.");
     message.channel.send ("Confirmation envoyée !");
-let memberRole = message.member.guild.roles.find("name", "Producteur ✔️");
-console.dir(message.member);
-  message.member.addRole(memberRole);    
-
+    
+    let memberRole = message.member.guild.roles.find("name", "Producteur ✔️");
+    message.member.addRole(memberRole);
 
 }
 
